34a0ae114a1daba8c357d43b27013fcb2313313d
Guy Rozen <HolyEggplant@gmail.com>
2018-03-02 00:07:19 +0200

    Some more pruning

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,13 +1,13 @@
-import { forall, assert, nat, Options } from 'jsverify';
-import { diagramArbitrary, withTime } from 'cyclejs-test-helpers';
-const htmlLooksLike = require('html-looks-like');
-const toHtml = require('snabbdom-to-html'); //snabbdom-to-html's typings are broken
+import { forall, assert, nat, Options } from 'jsverify'
+import { diagramArbitrary, withTime } from 'cyclejs-test-helpers'
+const htmlLooksLike = require('html-looks-like')
+const toHtml = require('snabbdom-to-html') //snabbdom-to-html's typings are broken
 
-import xs, { Stream } from 'xstream';
-import { mockDOMSource, VNode } from '@cycle/dom';
-import { mockTimeSource } from '@cycle/time';
-import onionify from 'cycle-onionify';
+import xs, { Stream } from 'xstream'
+import { mockDOMSource, VNode } from '@cycle/dom'
+import { mockTimeSource } from '@cycle/time'
+import onionify from 'cycle-onionify'
 
-import { App } from '../src/app';
+import { App } from '../src/app'
 
 // Maybe we'll add some tests here at some point...?
