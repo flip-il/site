c43f0c3f5fa7552f60534d93b8b5b9655e362234
Yoav Luft <yoav.luft@gmail.com>
2018-03-17 12:49:52 +0200

    Minor fixes

--- src/view/agenda.tsx
+++ src/view/agenda.tsx
@@ -13,7 +13,7 @@ export default function Agenda(state : AppState) : VNode {
         <div id={titleToAnchor('Agenda')}>
             <h1>Agenda:</h1>
             <h2>Coming Soon!</h2>
-            <p className={style(baseFont, {fontSize: '1.2rem'})}>
+            <p className={style(baseFont, {fontSize: '1.2rem', textAlign: 'center'})}>
                 In the meantime, our <a href="https://goo.gl/forms/hRzJCFvVNZ5Dd5qn1">call for papers</a> is open!
             </p>
             {/*{specificAgenda(AppData.Agenda.Gathering, state)}*/}
