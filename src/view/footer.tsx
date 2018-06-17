1cdf17c1561141493cd8a94381fd6eced58483d4
Guy Rozen <HolyEggplant@gmail.com>
2018-06-17 22:09:41 +0300

    Fixed the footer on Chrome

--- src/view/footer.tsx
+++ src/view/footer.tsx
@@ -1,6 +1,6 @@
 import { style } from 'typestyle'
 import * as csstips from 'csstips'
-import { VNode } from '@cycle/dom'
+import { VNode, table } from '@cycle/dom'
 
 import cycleLogo from '../../public/cyclejs_logo.svg'
 import typescriptLogo from '../../public/typescript.png'
@@ -8,22 +8,15 @@ import fbIcon from '../../public/FB-icon.png'
 import twitterIcon from '../../public/Twitter-icon.svg'
 
 export default function footer() : VNode {
-    const footerClass = style(csstips.horizontal, {
+    const footerClass = style({
         width: '100%',
         backgroundColor: 'black',
         padding: '0.5em'
     })
     const cycleJSClass = style({width: '26px', height: '26px', verticalAlign: 'bottom'})
     const typescriptClass = cycleJSClass
-    const codeOfConductClass = style({
-        marginTop: 'auto',
-        marginBottom: 'auto'
-    })
-    const techStackClass = style({marginRight: 'auto'})
     const contactClass = style({
-        marginLeft: 'auto',
-        marginTop: 'auto',
-        marginBottom: 'auto',
+        textAlign: 'right',
         $nest: {'a': {marginLeft: '4px', marginRight: '4px'}}
     })
     const iconClass = {
@@ -36,25 +29,19 @@ export default function footer() : VNode {
     }
     const emailIcon = style(iconClass, {display: 'inline-block'})
     const twitterIconClass = style({backgroundImage: `url(${twitterIcon})`}, iconClass, {display: 'inline-block'})
-    const facebookIconClass = style({backgroundImage: `url(${fbIcon})`}, iconClass, {display: 'inline-block'})
+    const facebookIconClass = style({ backgroundImage: `url(${fbIcon})` }, iconClass, { display: 'inline-block' })
 
     return (
-        <div className={footerClass}>
-            <div className={techStackClass}>
-                <p>Made with ♥ using <a href='https://cycle.js.org/'>cyclejs <img className={cycleJSClass}
-                                                                                  src={cycleLogo}/></a>, <a
-                    href='https://www.typescriptlang.org/'>typescript <img className={typescriptClass}
-                                                                           src={typescriptLogo}/></a> and <a
-                    href='http://typestyle.io/#/'>typestyle 🌹</a></p>
-            </div>
-            <div className={contactClass}>
-                <a href={'mailto:info@flip-il.org'}><span className={emailIcon}>📧</span>info@flip-il.org</a>
-                <a href={'https://twitter.com/FLIPCon'} target='_blank'><span className={twitterIconClass}/>@FLIPCon</a>
-                <a href={'https://www.facebook.com/FLIPcon/'} target='_blank'><span className={facebookIconClass}/>Facebook</a>
-            </div>
-            <div className={codeOfConductClass}>
-                <a href='/code-of-conduct.pdf' target='_blank'>code of conduct</a>
-            </div>
-        </div>
+        <table className={footerClass}>
+            <tr>
+                <td>Made with ♥ using <a href='https://cycle.js.org/'>cyclejs <img className={cycleJSClass} src={cycleLogo} /></a>, <a href='https://www.typescriptlang.org/'>typescript <img className={typescriptClass} src={typescriptLogo} /></a> and <a href='http://typestyle.io/#/'>typestyle 🌹</a></td>
+                <td className={contactClass}>
+                    <a href={'mailto:info@flip-il.org'}><span className={emailIcon}>📧</span>info@flip-il.org</a>
+                    <a href={'https://twitter.com/FLIPCon'} target='_blank'><span className={twitterIconClass} />@FLIPCon</a>
+                    <a href={'https://www.facebook.com/FLIPcon/'} target='_blank'><span className={facebookIconClass} />Facebook</a>
+                    <a href='/code-of-conduct.pdf' target='_blank'>code of conduct</a>
+                </td>
+            </tr>
+        </table>
     )
 }
